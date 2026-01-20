ff6e6e01b821cad81f35510baef689687fddc816
Lume001 <Amarinho728@gmail.com>
2026-01-19 23:22:20 -0300

    primeiro commit pokedex

---
+++ src/main/java/dev/java1/Pokedex/PokedexController.java
@@ -0,0 +1,16 @@
+package dev.java1.Pokedex;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PokedexController {
+
+    @GetMapping("/testerota")
+    public String testeRota(){
+        return"Este é um teste de rota";
+    }
+
+}
